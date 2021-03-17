1c67cd7b665bd8c7bfc2677e563afba11d52b887
Juan Carlos Perez <jcperezrom@gmail.com>
2021-03-16 21:39:49 -0600

    docs agregado

---
+++ docs/precache-manifest.9677d5f6014dfa9613987e076d342b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27ac04550096b90982704837e1cd88d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1821e4e69db9d046fb59",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1821e4e69db9d046fb59",
+    "url": "/static/js/main.6b8bb454.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
